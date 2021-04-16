ebbdd4586bad2d8384ab96d4295b0bb560bd5465
Kinnera Tummalacherla <kinneratummalacherla@gmail.com>
2021-04-16 16:22:07 +0530

    New one

--- P1.java
+++ P1.java
@@ -1,7 +1,18 @@
+import java.util.*;
 class P1
 {
 	public static void main(String args[])
 	{
 		System.out.println("Hii");
+		Scanner sc = new Scanner(System.in);
+		int a =sc.nextInt();
+		if(a > 5)
+		{
+			System.out.println("Hii");
+		}
+		else
+		{
+			System.out.println("Hello");
+		}
 	}
 }
